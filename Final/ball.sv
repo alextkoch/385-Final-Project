44909680341e5b82766bbbd0fadf96a66cfb1e8c
alextkoch <55167484+alextkoch@users.noreply.github.com>
2021-11-16 16:56:55 -0600

    Changed references of ball to tank in all used code. Commented out movement for the time being (and therefore haven't changed it yet)

--- Final/ball.sv
+++ Final/ball.sv
@@ -17,98 +17,80 @@
 
 module  ball ( input Reset, frame_clk,
 					input [7:0] keycode,
-               output [9:0]  BallX, BallY, BallS );
+	      output [9:0]  TankX, TankY);
     
-    logic [9:0] Ball_X_Pos, Ball_X_Motion, Ball_Y_Pos, Ball_Y_Motion, Ball_Size;
+	logic [9:0] Tank_X_Pos, Tank_X_Motion, Tank_Y_Pos, Tank_Y_Motion;
 	 
-    parameter [9:0] Ball_X_Center=320;  // Center position on the X axis
-    parameter [9:0] Ball_Y_Center=240;  // Center position on the Y axis
-    parameter [9:0] Ball_X_Min=0;       // Leftmost point on the X axis
-    parameter [9:0] Ball_X_Max=639;     // Rightmost point on the X axis
-    parameter [9:0] Ball_Y_Min=0;       // Topmost point on the Y axis
-    parameter [9:0] Ball_Y_Max=479;     // Bottommost point on the Y axis
-    parameter [9:0] Ball_X_Step=1;      // Step size on the X axis
-    parameter [9:0] Ball_Y_Step=1;      // Step size on the Y axis
-
-    assign Ball_Size = 4;  // assigns the value 4 as a 10-digit binary number, ie "0000000100"
+	//Psuedo Middle of the Bottom of the Screen (easy w)
+	parameter [9:0] Tank_X_Int= 350;  // Leftmost position on the X axis upon reset (starting position essentially)
+	parameter [9:0] Tank_Y_Int= 500;       // Topmost point on the Y axis upon reset
+	parameter [9:0] Tank_X_Step=1;      // Step size on the X axis 
+	parameter [9:0] Tank_Y_Step=1;      // Step size on the Y axis 
+			//This step size thing is VERY subject to change, I don't like it at all. Hard to gage w/o movement
    
     always_ff @ (posedge Reset or posedge frame_clk )
-    begin: Move_Ball
+    begin: Move_
         if (Reset)  // Asynchronous Reset
         begin 
-            Ball_Y_Motion <= 10'd0; //Ball_Y_Step;
-				Ball_X_Motion <= 10'd0; //Ball_X_Step;
-				Ball_Y_Pos <= Ball_Y_Center;
-				Ball_X_Pos <= Ball_X_Center;
+            			Tank_Y_Motion <= 10'd0;
+				Tank_X_Motion <= 10'd0;
+				Tank_Y_Pos <= Tank_Y_Int;
+				Tank_X_Pos <= Tank_X_Int;
         end
            
-        else 
-        begin 
-				 if ( (Ball_Y_Pos + Ball_Size) >= Ball_Y_Max )  // Ball is at the bottom edge, BOUNCE!
-					  Ball_Y_Motion <= (~ (Ball_Y_Step) + 1'b1);  // 2's complement.
+//         else 
+//         begin 
+// 				 if ( (Ball_Y_Pos + Ball_Size) >= Ball_Y_Max )  // Ball is at the bottom edge, BOUNCE!
+// 					  Ball_Y_Motion <= (~ (Ball_Y_Step) + 1'b1);  // 2's complement.
 					  
-				 else if ( (Ball_Y_Pos - Ball_Size) <= Ball_Y_Min )  // Ball is at the top edge, BOUNCE!
-					  Ball_Y_Motion <= Ball_Y_Step;
+// 				 else if ( (Ball_Y_Pos - Ball_Size) <= Ball_Y_Min )  // Ball is at the top edge, BOUNCE!
+// 					  Ball_Y_Motion <= Ball_Y_Step;
 					  
-				  else if ( (Ball_X_Pos + Ball_Size) >= Ball_X_Max )  // Ball is at the Right edge, BOUNCE!
-					  Ball_X_Motion <= (~ (Ball_X_Step) + 1'b1);  // 2's complement.
+// 				  else if ( (Ball_X_Pos + Ball_Size) >= Ball_X_Max )  // Ball is at the Right edge, BOUNCE!
+// 					  Ball_X_Motion <= (~ (Ball_X_Step) + 1'b1);  // 2's complement.
 					  
-				 else if ( (Ball_X_Pos - Ball_Size) <= Ball_X_Min )  // Ball is at the Left edge, BOUNCE!
-					  Ball_X_Motion <= Ball_X_Step;
+// 				 else if ( (Ball_X_Pos - Ball_Size) <= Ball_X_Min )  // Ball is at the Left edge, BOUNCE!
+// 					  Ball_X_Motion <= Ball_X_Step;
 					  
-				 else 
-					  Ball_Y_Motion <= Ball_Y_Motion;  // Ball is somewhere in the middle, don't bounce, just keep moving
+// 				 else 
+// 					  Ball_Y_Motion <= Ball_Y_Motion;  // Ball is somewhere in the middle, don't bounce, just keep moving
 					  
 				 
-				 case (keycode)
-					8'h04 : begin
+// 				 case (keycode)
+// 					8'h04 : begin
 
-								Ball_X_Motion <= -1;//A
-								Ball_Y_Motion<= 0;
-							  end
+// 								Ball_X_Motion <= -1;//A
+// 								Ball_Y_Motion<= 0;
+// 							  end
 					        
-					8'h07 : begin
+// 					8'h07 : begin
 								
-					        Ball_X_Motion <= 1;//D
-							  Ball_Y_Motion <= 0;
-							  end
+// 					        Ball_X_Motion <= 1;//D
+// 							  Ball_Y_Motion <= 0;
+// 							  end
 
 							  
-					8'h16 : begin
+// 					8'h16 : begin
 
-					        Ball_Y_Motion <= 1;//S
-							  Ball_X_Motion <= 0;
-							 end
+// 					        Ball_Y_Motion <= 1;//S
+// 							  Ball_X_Motion <= 0;
+// 							 end
 							  
-					8'h1A : begin
-					        Ball_Y_Motion <= -1;//W
-							  Ball_X_Motion <= 0;
-							 end	  
-					default: ;
-			   endcase
+// 					8'h1A : begin
+// 					        Ball_Y_Motion <= -1;//W
+// 							  Ball_X_Motion <= 0;
+// 							 end	  
+// 					default: ;
+// 			   endcase
 				 
-				 Ball_Y_Pos <= (Ball_Y_Pos + Ball_Y_Motion);  // Update ball position
-				 Ball_X_Pos <= (Ball_X_Pos + Ball_X_Motion);
-			
-			
-	  /**************************************************************************************
-	    ATTENTION! Please answer the following quesiton in your lab report! Points will be allocated for the answers!
-		 Hidden Question #2/2:
-          Note that Ball_Y_Motion in the above statement may have been changed at the same clock edge
-          that is causing the assignment of Ball_Y_pos.  Will the new value of Ball_Y_Motion be used,
-          or the old?  How will this impact behavior of the ball during a bounce, and how might that 
-          interact with a response to a keypress?  Can you fix it?  Give an answer in your Post-Lab.
-      **************************************************************************************/
-      
-			
-		end  
+// 				 Ball_Y_Pos <= (Ball_Y_Pos + Ball_Y_Motion);  // Update ball position
+// 				 Ball_X_Pos <= (Ball_X_Pos + Ball_X_Motion);
+
+// 		end  
     end
        
-    assign BallX = Ball_X_Pos;
+    assign TankX = Tank_X_Pos;
    
-    assign BallY = Ball_Y_Pos;
-   
-    assign BallS = Ball_Size;
-    
+    assign TankY = Tank_Y_Pos;   
 
 endmodule
