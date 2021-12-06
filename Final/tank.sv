dc95db29c6793f3fa8021df371feda9cec64d21f
alextkoch <55167484+alextkoch@users.noreply.github.com>
2021-12-06 16:49:49 -0600

    changed tank from pixel to tile

--- Final/tank.sv
+++ Final/tank.sv
@@ -18,21 +18,16 @@
 module  tank ( input Reset, frame_clk, player,
 					input [7:0] keycode,
 	      input int map[300],
-	      output [9:0]  TankX, TankY);
-    
-	logic [9:0] Tank_X_Pos, Tank_X_Motion, Tank_Y_Pos, Tank_Y_Motion, Tank_X_Pot, Tank_Y_Pot;
-	logic [5:0] potX, potY;
+	      output int  TankX, TankY);
+
+	int Tank_X_Pos, Tank_X_Motion, Tank_Y_Pos, Tank_Y_Motion, Tank_X_Pot, Tank_Y_Pot;
+	int potX, potY;
 	int nextTile, valid;
 	 
-	//Psuedo Middle of the Bottom of the Screen (easy w)
-	parameter [9:0] Tank1_X_Int= 32;  // Leftmost position on the X axis upon reset (starting position essentially)
-	parameter [9:0] Tank1_Y_Int= 416;       // Topmost point on the Y axis upon reset
-	parameter [9:0] Tank2_X_Int= 576;  // Leftmost position on the X axis upon reset (starting position essentially)
-	parameter [9:0] Tank2_Y_Int= 32;       // Topmost point on the Y axis upon reset
-	
-	parameter [9:0] Tank_X_Step=1;      // Step size on the X axis 
-	parameter [9:0] Tank_Y_Step=1;      // Step size on the Y axis 
-			//This step size thing is VERY subject to change, I don't like it at all. Hard to gage w/o movement
+	parameter int Tank1_X_Int= 1;  // Leftmost position on the X axis upon reset (starting position essentially)
+	parameter int Tank1_Y_Int= 13;       // Topmost point on the Y axis upon reset
+	parameter int Tank2_X_Int= 18;  // Leftmost position on the X axis upon reset (starting position essentially)
+	parameter int Tank2_Y_Int= 1;       // Topmost point on the Y axis upon reset
    
     always_ff @ (posedge Reset or posedge frame_clk )
     begin: Move_
@@ -40,15 +35,15 @@ module  tank ( input Reset, frame_clk, player,
         begin 
 		if (player) //low means we're dealing with player 1
 			begin
-				Tank_Y_Motion <= 10'd0;
-				Tank_X_Motion <= 10'd0;
+				Tank_Y_Motion <= 0;
+				Tank_X_Motion <= 0;
 				Tank_Y_Pos <= Tank1_Y_Int;
 				Tank_X_Pos <= Tank1_X_Int;
 			end
 		else
 			begin
-				Tank_Y_Motion <= 10'd0;
-				Tank_X_Motion <= 10'd0;
+				Tank_Y_Motion <= 0;
+				Tank_X_Motion <= 0;
 				Tank_Y_Pos <= Tank2_Y_Int;
 				Tank_X_Pos <= Tank2_X_Int;
 			end
@@ -124,8 +119,8 @@ module  tank ( input Reset, frame_clk, player,
 				end
 					
 		end 			
-			potX[5:0] = Tank_X_Pos[9:5] + Tank_X_Motion[9:5];
-			potY[5:0] = Tank_Y_Pos[9:5] + Tank_Y_Motion[9:5];
+			potX = Tank_X_Pos + Tank_X_Motion;
+			potY = Tank_Y_Pos + Tank_Y_Motion;
 				
 			nextTile = potY * 20 + potX;
 			
@@ -141,9 +136,10 @@ module  tank ( input Reset, frame_clk, player,
 							Tank_X_Pos <= Tank_X_Pos;
 							Tank_Y_Pos <= Tank_Y_Pos;
 						end
-			TankX <= Tank_X_Pos;
-			TankY <= Tank_Y_Pos;
     end
     end 
 
+	assign	TankX = Tank_X_Pos;
+	assign	TankY = Tank_Y_Pos;
+	
 endmodule
