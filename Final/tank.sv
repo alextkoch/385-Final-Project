1b7adde4c04d9e5ccf2fd82bddae0065de17db95
gavinwzl <94490214+gavinwzl@users.noreply.github.com>
2021-12-09 00:24:46 -0600

    Update tank.sv

--- Final/tank.sv
+++ Final/tank.sv
@@ -2,8 +2,12 @@
 module  tank ( input Reset, frame_clk, player,
 					input [7:0] keycode,
 	      input int map[300],
+			input logic loss, gotHit,
+			input int enemyX, enemyY,
 	      output int  TankX, TankY, BulX, BulY,
-			output int change);
+			output int change,
+			output logic win, hitted
+			);
 
 	int Tank_X_Pos, Tank_X_Motion, Tank_Y_Pos, Tank_Y_Motion, Tank_X_Pot, Tank_Y_Pot;
 	int potX_tank, potY_tank;
@@ -15,7 +19,12 @@ module  tank ( input Reset, frame_clk, player,
 	
 	int dir;
 	
+	logic won;
+	
 	logic is_bul, attempt;
+	
+	int whereHeAt;
+	assign whereHeAt = enemyY * 20 + enemyX;
 	
 	parameter int Tank1_X_Int= 1;  // Leftmost position on the X axis upon reset (starting position essentially)
 	parameter int Tank1_Y_Int= 13;       // Topmost point on the Y axis upon reset
@@ -30,8 +39,8 @@ module  tank ( input Reset, frame_clk, player,
 		dir <= 0;
 		Bul_Y_Motion <= 0;
 		Bul_X_Motion <= 0;
-
-		
+		won <= 1'b0;
+		win <= 1'b0;
 		if (player) //low means we're dealing with player 1
 			begin
 				Tank_Y_Motion <= 0;
@@ -54,8 +63,50 @@ module  tank ( input Reset, frame_clk, player,
            
        else 
         	begin 				  
-			
-			if(player) //distinction here is important, player 1 uses WASD, player 2 used the arrow keys
+			if (loss) begin
+				Tank_Y_Motion <= 0;
+				Tank_X_Motion <= 0;
+				Tank_Y_Pos <= -1;
+				Tank_X_Pos <= -1;
+				Bul_Y_Pos <= -1;
+				Bul_X_Pos <= -1;
+				win <= 1'b0;
+			end
+			else if (won) begin
+				Tank_Y_Motion <= 0;
+				Tank_X_Motion <= 0;
+				Tank_Y_Pos <= 7;
+				Tank_X_Pos <= 9;
+				Bul_Y_Pos <= -1;
+				Bul_X_Pos <= -1;
+				win <= 1'b1;
+				won <= 1'b0;
+			end
+			else if (gotHit) begin
+				win <= 1'b0;
+				Tank_Y_Motion <= 0;
+				Tank_X_Motion <= 0;
+				Bul_Y_Pos <= -1;
+				Bul_X_Pos <= -1;
+				won <= 1'b0;
+				if (player) begin
+					Tank_Y_Pos <= Tank1_Y_Int;
+					Tank_X_Pos <= Tank1_X_Int;
+					Bul_Y_Pos <= -1;
+					Bul_X_Pos <= -1;
+				end
+				else begin
+					Tank_Y_Motion <= 0;
+					Tank_X_Motion <= 0;
+					Tank_Y_Pos <= Tank2_Y_Int;
+					Tank_X_Pos <= Tank2_X_Int;
+					Bul_Y_Pos <= -1;
+					Bul_X_Pos <= -1;
+					end
+			end
+			else begin
+				win <= 1'b0;
+				if(player) //distinction here is important, player 1 uses WASD, player 2 used the arrow keys
 				begin
 					 case (keycode)
 					8'h04 : begin
@@ -176,43 +227,70 @@ module  tank ( input Reset, frame_clk, player,
 						end
 						
 
+			end			
+
 	    if (is_bul) begin
 //	    	Bul_X_Motion <= Bul_X_Motion;
 //		Bul_Y_Motion <= Bul_Y_Motion;
 //		Bul_X_Pos <= Bul_X_Pos + Bul_X_Motion;
 //		Bul_Y_Pos <= Bul_Y_Pos + Bul_Y_Motion;
 //		is_bul <= 1; working movement
-			
-		case (map[Bul_Y_Pos * 20 + Bul_X_Pos])
-			0 : begin
-					change <= 0;
-					Bul_X_Motion <= Bul_X_Motion;
-					Bul_Y_Motion <= Bul_Y_Motion;
-					Bul_X_Pos <= Bul_X_Pos + Bul_X_Motion;
-					Bul_Y_Pos <= Bul_Y_Pos + Bul_Y_Motion;
-					is_bul <= 1;
-				 end
-			1 : begin
-					change <= 0;
-					Bul_X_Pos <= -1;
-					Bul_Y_Pos <= -1;
-					is_bul <= 0;
-				 end
-			2 : begin
-					change <= Bul_Y_Pos * 20 + Bul_X_Pos;
-					Bul_X_Pos <= -1;
-					Bul_Y_Pos <= -1;
-					is_bul <= 0;
-				 end
-			default begin
-					change <= 0;
-					Bul_X_Pos <= -1;
-					Bul_Y_Pos <= -1;
-					is_bul <= 0;
-				 end
-			endcase
+			if ((Bul_Y_Pos * 20 + Bul_X_Pos) == whereHeAt) begin
+						hitted <= 1'b1;
+						change <= 0;
+						Bul_X_Pos <= -1;
+						Bul_Y_Pos <= -1;
+						is_bul <= 0;
+					end
+					else begin
+						hitted <= 1'b0;
+						case (map[Bul_Y_Pos * 20 + Bul_X_Pos])
+							0 : begin
+									change <= 0;
+									Bul_X_Motion <= Bul_X_Motion;
+									Bul_Y_Motion <= Bul_Y_Motion;
+									Bul_X_Pos <= Bul_X_Pos + Bul_X_Motion;
+									Bul_Y_Pos <= Bul_Y_Pos + Bul_Y_Motion;
+									is_bul <= 1;
+								 end
+							1 : begin
+									change <= 0;
+									Bul_X_Pos <= -1;
+									Bul_Y_Pos <= -1;
+									is_bul <= 0;
+								 end
+							2 : begin
+									change <= Bul_Y_Pos * 20 + Bul_X_Pos;
+									Bul_X_Pos <= -1;
+									Bul_Y_Pos <= -1;
+									is_bul <= 0;
+								 end
+							3 : begin
+									Bul_X_Pos <= -1;
+									Bul_Y_Pos <= -1;
+									is_bul <= 0;
+									if (!player)
+										won <= 1'b1;
+								 end
+							4 : begin
+									Bul_X_Pos <= -1;
+									Bul_Y_Pos <= -1;
+									is_bul <= 0;
+									if (player)
+										won <= 1'b1;
+								 end	 
+							default begin
+									change <= 0;
+									Bul_X_Pos <= -1;
+									Bul_Y_Pos <= -1;
+									is_bul <= 0;
+									
+								 end
+							endcase
+			end //end of if not at the enemy
 	    end
 	    else begin
+			 hitted <= 1'b0;
 		    change <= 0;
 		    if (attempt) begin
 			    case (dir)
